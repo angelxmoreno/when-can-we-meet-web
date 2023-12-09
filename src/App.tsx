1dd5ac3abacd6f06f2bc473fed1b8673da6e66a0
Angel S. Moreno <angelxmoreno@gmail.com>
2023-12-08 23:25:05 -0500

    installed toast service

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { router } from '@app/config/routes';
 import React, { FC } from 'react';
 import { RouterProvider } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 const App: FC = () => (
     <React.StrictMode>
         <RouterProvider router={router} />
+        <ToastContainer />
     </React.StrictMode>
 );
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
